c001b6400098153a75d1d2d5b4ee98935cf0190a
Shahwaiz Islam <s1404islam@gmail.com>
2024-07-24 01:06:47 +0530

    Auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setEndpointProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another method
+        //if account banaya then login bhi karado
+        return this.login({ email, password });
+      } else {
+        //null
+        return userAccount;
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;
