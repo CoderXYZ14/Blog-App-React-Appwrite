fb831079450af74264f816057a1563e15ea86da6
Shahwaiz Islam <s1404islam@gmail.com>
2024-07-24 03:07:53 +0530

    Tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import "./App.css";
-
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return <h1>Hello, React!</h1>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+  return !loading ? <div className="min-h-sc"></div> : null;
 }
 
 export default App;
